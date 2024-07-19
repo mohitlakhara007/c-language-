c53c9397fe2f9cc21dedd2fa393b1599e3bfd61b
mohitlakhara007 <mohitlakhara007061@gmail.com>
2024-07-19 16:32:03 +0530

    Add files via upload

---
+++ lec.1/bio.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+
+main()
+{
+	printf("Name=mohit lakhara\n");
+	printf("Father=Omprakash lakhara\n");
+	printf("Mother=sarita lakhara\n");
+	printf("Number=9898972439\n");
+	printf("Email=mohitlakhara@gmail.com\n");
+}
