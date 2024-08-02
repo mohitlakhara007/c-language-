e18080ded28758442069434633b1d20cda8bc829
mohitlakhara007 <mohitlakhara007061@gmail.com>
2024-08-02 14:40:34 +0530

    Add files via upload

---
+++ Control Structure/maximum.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+	int a;
+	printf("enter the A: ");
+	scanf("%d",&a);
+	
+	int b;
+	printf("enter the B: ");
+	scanf("%d",&b);
+	
+	(a>b)?printf("a is maximum") : printf("b is maximum");
+	
+}
+
+
+
+
+
---
+++ Control Structure/minimum.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+	int a;
+	printf("enter the A: ");
+	scanf("%d",&a);
+	
+	int b;
+	printf("enter the B: ");
+	scanf("%d",&b);
+	
+	(a<b)?printf("a is minimum") : printf("b is minimum");
+	
+}
+
