09167cafb4a8d3a06e6788d4e7bb16a0ac3ca12a
mohitlakhara007 <mohitlakhara007061@gmail.com>
2024-08-06 14:10:10 +0530

    Add files via upload

---
+++ Control Structure/minimum.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+	int a;
+	printf("enter the A: ");
+	scanf("%d",&a);
+	
+	int b;
+	printf("enter the B: ");
+	scanf("%d",&b);
+	
+	(a<b)?printf("A is minimum") : printf("B is minimum");
+	
+}
+
